8bbe3c5e40fb250e267cc3c32d61b01f06444e20
serkan <s.akin@student.utwente.nl>
2022-11-06 18:32:57 +0100

    updated all commands (missing sending flight data)

--- falconControl/drone/falcon.py
+++ falconControl/drone/falcon.py
@@ -1,34 +1,21 @@
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+from pymavlink import mavutil  # Needed for command message definitions
 import time
+import math
 
-from dronekit import connect, VehicleMode
-from pymavlink import mavutil
-
-
+drone = ""
 # connects to the drone or the simulator, according to the input we give
-def droneConnect(ip):
-    if ip == "Yes":
-        test = True
-    else:
-        test = False
-    if test:
-        print("Connected to the simulator, for help run the command\n<drone -h>")
-        vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
-        print ("Connection complete")
-        return vehicle
-    else:
-        print("Connecting to drone")
-        return connect(ip, wait_ready=True)
+def droneConnect(connection_string, baud):
+    print("Connecting to drone")
+    vehicle = connect(connection_string, baud=baud, wait_ready=True)
+    drone = vehicle
+    print("connected")
+    vehicle.airspeed = 3
+    return vehicle
 
 
 # arms the drone
 def arm(vehicle):
-    #------
-    if vehicle == "test":
-        print("test arm")
-        f = open('/home/drone/.local/lib/python2.7/site-packages/falconControl/drone/test.txt', 'w')
-        f.write('test arm\n')
-        return
-    #------
     print("Basic pre-arm checks")
     # Don't try to arm until autopilot is ready
     while not vehicle.is_armable:
@@ -46,29 +33,10 @@ def arm(vehicle):
 
 
 def takeoff(vehicle, aTargetAltitude):
-    #------
-    if vehicle == "test":
-        print("test take of with")
-
-        return
-    #------
+    arm(vehicle)
     if not vehicle.armed:
-        print("Falcon needs to be armed first!!(drone -h for more information)")
+        print("Falcon needs to be armed first")
         return
-    print("Basic pre-arm checks")
-    # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
-    print("Arming motors")
-    # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True
-    # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
-        print(" Waiting for arming...")
-        time.sleep(1)
-    print ("Falcon is armed, ready to take off!")
     print("Taking off!")
     vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
     # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
@@ -82,65 +50,48 @@ def takeoff(vehicle, aTargetAltitude):
         time.sleep(1)
 
 
-def takeoffDefault(vehicle):
-    #------
-    if vehicle == "test":
-        print("test take of without")
-        f = open('/home/drone/.local/lib/python2.7/site-packages/falconControl/drone/test.txt', 'w')
-        f.write('test takeoff wt\n')
-        return
-    #------
-    print("Basic pre-arm checks")
-    # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
-    print("Arming motors")
-    # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True
-    # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
-        print(" Waiting for arming...")
-        time.sleep(1)
-    print ("Falcon is armed, ready to take off!")
-    if not vehicle.armed:
-        print("Falcon needs to be armed first!!(drone -h for more information)")
-        return
-    print("Taking off!")
-    vehicle.simple_takeoff(1)  # Take off to target altitude
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.simple_takeoff will execute immediately).
-    while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        # Break and return from function just below target altitude.
-        if vehicle.location.global_relative_frame.alt >= 5 * 0.95:
-            print("Reached target altitude")
-            break
-        time.sleep(1)
-
 # given the directions and the time, drone will move accordingly
-def move(vehicle, direction_x, direction_y, direction_z, duration):
-    """
-       Move vehicle in direction based on specified velocity vectors.
+def move_horizontal(vehicle, direction, metres, gotoFunction = drone.simple_goto):
+    yaw = vehicle.attitude.yaw
+    if 0 <= yaw <= 90:
+        dir_comp = "N"
+    elif 90 < yaw <= 180:
+        dir_comp = "E"
+    elif 180 < yaw <= 270:
+        dir_comp = "S"
+    else:
+        dir_comp = "W"
+    if (direction == "f" and dir_comp == "N") or (direction == "r" and dir_comp == "W") or (direction == "l" and dir_comp == "E") or (direction == "b" and dir_comp == "S"):
+        d_north = metres
+        d_east = 0
+    elif (direction == "f" and dir_comp == "S") or (direction == "r" and dir_comp == "E") or (direction == "l" and dir_comp == "W") or (direction == "b" and dir_comp == "N"):
+        d_north = metres * -1
+        d_east = 0
+    elif (direction == "f" and dir_comp == "E") or (direction == "r" and dir_comp == "N") or (direction == "l" and dir_comp == "S") or (direction == "b" and dir_comp == "W"):
+        d_north = 0
+        d_east = metres
+    else:
+        d_north = 0
+        d_east = metres * -1
+    current_location = vehicle.location.global_relative_frame
+    target_location = get_location_metres(current_location, d_north, d_east)
+    target_distance = get_distance_metres(current_location, target_location)
+    gotoFunction(target_location)
 
-       This uses the SET_POSITION_TARGET_GLOBAL_INT command with type mask enabling only
-       velocity components
-       (http://dev.ardupilot.com/wiki/copter-commands-in-guided-mode/#set_position_target_global_int).
+    # print "DEBUG: targetLocation: %s" % targetLocation
+    # print "DEBUG: targetLocation: %s" % targetDistance
 
-       Note that from AC3.3 the message should be re-sent every second (after about 3 seconds
-       with no message the velocity will drop back to zero). In AC3.2.1 and earlier the specified
-       velocity persists until it is canceled. The code below should work on either version
-       (sending the message multiple times does not cause problems).
+    while vehicle.mode.name == "GUIDED":  # Stop action if we are no longer in guided mode.
+        # print "DEBUG: mode: %s" % vehicle.mode.name
+        remainingDistance = get_distance_metres(vehicle.location.global_relative_frame, target_location)
+        print("Distance to target: ", remainingDistance)
+        if remainingDistance <= target_distance * 0.01:  # Just below target, in case of undershoot.
+            print("Reached target")
+            break
+        time.sleep(2)
 
-       See the above link for information on the type_mask (0=enable, 1=ignore).
-       At time of writing, acceleration and yaw bits are ignored.
-       """
-    #------
-    if vehicle == "test":
-        print("test move")
-        return
-    #------
+
+def move_vertical(vehicle, movement, duration):
     msg = vehicle.message_factory.set_position_target_global_int_encode(
         0,  # time_boot_ms (not used)
         0, 0,  # target system, target component
@@ -150,9 +101,9 @@ def move(vehicle, direction_x, direction_y, direction_z, duration):
         0,  # lon_int - Y Position in WGS84 frame in 1e7 * meters
         0,  # alt - Altitude in meters in AMSL altitude(not WGS84 if absolute or relative)
         # altitude above terrain if GLOBAL_TERRAIN_ALT_INT
-        direction_x,  # X velocity in NED frame in m/s
-        direction_y,  # Y velocity in NED frame in m/s
-        direction_z,  # Z velocity in NED frame in m/s
+        0,  # X velocity in NED frame in m/s
+        0,  # Y velocity in NED frame in m/s
+        movement,  # Z velocity in NED frame in m/s
         0, 0, 0,  # afx, afy, afz acceleration (not supported yet, ignored in GCS_Mavlink)
         0, 0)  # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
 
@@ -161,48 +112,53 @@ def move(vehicle, direction_x, direction_y, direction_z, duration):
         vehicle.send_mavlink(msg)
         time.sleep(1)
 
-# Land to current location
+
+    # Land to current location
 def land(vehicle):
-    #------
-    if vehicle == "test":
-        print("test land")
-        return
-    #------
     vehicle.mode = VehicleMode("LAND")
 
-# Horizontal rotate
-def rotate(vehicle, direction, degree):
-    """
-        Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
 
-        This method sets an absolute heading by default, but you can set the `relative` parameter
-        to `True` to set yaw relative to the current yaw heading.
-
-        By default the yaw of the vehicle will follow the direction of travel. After setting
-        the yaw using this function there is no way to return to the default yaw "follow direction
-        of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
-
-        For more information see:
-        http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
-        """
-    #------
-    if vehicle == "test":
-        print("test rotate")
-        return
-    #------
-    if direction[3:] == "right":
-        dir = 1
+def rotate(vehicle, direction):
+    if direction == "r":
+        rot_dir = 1
     else:
-        dir = -1
+        rot_dir = -1
     # create the CONDITION_YAW command using command_long_encode()
     msg = vehicle.message_factory.command_long_encode(
         0, 0,  # target system, target component
         mavutil.mavlink.MAV_CMD_CONDITION_YAW,  # command
         0,  # confirmation
-        degree,  # param 1, yaw in degrees
+        90,  # param 1, yaw in degrees
         0,  # param 2, yaw speed deg/s
-        dir,  # param 3, direction -1 ccw, 1 cw
+        rot_dir,  # param 3, direction -1 ccw, 1 cw
         1,  # param 4, relative offset 1, absolute angle 0
         0, 0, 0)  # param 5 ~ 7 not used
     # send command to vehicle
     vehicle.send_mavlink(msg)
+
+
+def get_location_metres(original_location, d_north, d_east):
+    earth_radius = 6378137.0  # Radius of "spherical" earth
+    # Coordinate offsets in radians
+    dLat = d_north / earth_radius
+    dLon = d_east / (earth_radius * math.cos(math.pi * original_location.lat / 180))
+
+    # New position in decimal degrees
+    new_lat = original_location.lat + (dLat * 180 / math.pi)
+    new_lon = original_location.lon + (dLon * 180 / math.pi)
+    if type(original_location) is LocationGlobal:
+        target_location = LocationGlobal(new_lat, new_lon, original_location.alt)
+    elif type(original_location) is LocationGlobalRelative:
+        target_location = LocationGlobalRelative(new_lat, new_lon, original_location.alt)
+    else:
+        raise Exception("Invalid Location object passed")
+    return target_location
+
+
+def get_distance_metres(a_location1, a_location2):
+    dlat = a_location2.lat - a_location1.lat
+    dlong = a_location2.lon - a_location1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+
+
--- falconControl/drone/main.py
+++ falconControl/drone/main.py
@@ -1,73 +1,57 @@
 import falcon
-import time
 
-helpMenu = "Welcome to the Falcon Control Center(TUI)\n" \
-           "Here are the supported commands:\n" \
-           "arm: arm the drone safely\n" \
-           "takeoff <x>: takeoff with given value(x) 5 is default\n" \
-           "land: to land\n" \
-           "forward/back/right/left <x>: move to the given direction given values\n" \
-           "rot_right/rot_left <x>: rotate given degrees(x)\n"
 
 def main():
     run("connect\n")
     commands()
+
 
 def run(line):
     f = open('/home/drone/.local/lib/python2.7/site-packages/falconControl/drone/test.txt', 'a')
     f.write(line)
     f.close()
 
+
 def commands():
-    #inp = raw_input("Is this a simulation run?(Yes or ip)")
-    # for now to skip the connection phase and test
-    land = True
-    count = 0
-    connected = False
-    with open('/home/drone/.local/lib/python2.7/site-packages/falconControl/drone/commands.txt', 'r+') as file:
-        while land:
-            count = count + 1
-            line = file.readline()
-            if line:
-                if not connected  and line != "s\n":
-                    #vehicle = falcon.droneConnect(line)
-                    connected = False
-                elif line == "s\n":
-                    print(line)
-                    vehicle = "test"
+    land = True  # continue reading lines until drone lands
+    connected = False  # check whether the connection is done
+    with open('/home/drone/.local/lib/python2.7/site-packages/falconControl/drone/commands.txt',
+              'r+') as fi:  # open file to write and read
+        while land:  # while we have not received the land command which will end the program
+            line = fi.readline()  # read from commands.txt
+            if line:  # when new command received
+                if not connected:
+                    if line != "s\n":  # actual connection over tcp
+                        vehicle = falcon.droneConnect(line, 57600)
+                    else:  # testing
+                        print(line)
+                        vehicle = "test"
                     connected = True
                 else:
-                    command = line.split(':')
-                    # print the help menu
-                    if line == "drone -h":
-                        print(helpMenu)
-                    elif command[0] == "arm":
-                        print("arm")
-                        falcon.arm(vehicle)
-                    elif command[0] == "toff":
-                        run(line)
-                        file.write("toff\n")
-                        if len(command) == 2 and command[1] != "1":
-                            try:
-                                falcon.takeoff(vehicle, int(command[1]))
-                            except ValueError:
-                                print("ERROR: please enter a valid altidute for takeoff")
-                        else:
-                            falcon.takeoffDefault(vehicle)
-                    elif command[0] == "land":
-                        falcon.land(vehicle)
-                        land = False
-                    elif (len(command) == 2) and (
-                            command[0] == "forward" or command[0] == "back" or command[0] == "right" or command[
-                        0] == "left"):
-                        try:
-                            falcon.move(vehicle, command[0], int(command[1]))
-                        except ValueError:
-                            print("ERROR: please enter a valid value for movement")
-                    elif (len(command) == 2) and (command[0] == "rot_right" or command[0] == "rot_left"):
-                        try:
-                            falcon.rotate(vehicle, command[0], int(command[1]))
-                        except ValueError:
-                            print("ERROR: please enter a valid value for rotation")
+                    land = readCommands(line, vehicle)
+
+
+def readCommands(line, vehicle):
+    command = line.split(':')  # split the command according to protocol language
+    if command[0] == "toff":
+        falcon.takeoff(vehicle, command[1])
+        return False
+    elif command[0] == "land":
+        falcon.land(vehicle)
+        return True
+    elif command[0] == "move":
+        movement = command[1]
+        if movement == "u":
+            falcon.move_vertical(vehicle, int(command[2])/5 * -1, 5)
+        elif movement == "d":
+            falcon.move_vertical(vehicle, int(command[2])/5, 5)
+        elif movement == "o":
+            falcon.rotate(vehicle, command[2])
+        else:
+            falcon.move_horizontal(vehicle, movement, int(command[2]))
+        return False
+    elif command[0] == "sped":
+        vehicle.airspeed = int(command[2])
+
 
 main()
